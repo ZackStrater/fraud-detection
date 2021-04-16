a31d2d117168e1a78866661d09f07c5d0f556a25
Danielkennett <daniel.l.kennett@gmail.com>
2021-04-16 14:09:37 -0400

    more Flask

--- apps/app.py
+++ apps/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('quadratic.html')
+    return render_template('quadratic(copy).html')
 
 
 @app.route('/solve', methods=['POST'])
